1ac12f3ad284ee9aa349818f708f59f3359e1f19
seungh0 <will.seungho@gmail.com>
2020-12-29 23:55:31 +0900

    feat(nest): e2e test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('')
 export class AppController {
-  @Get('/ping')
+  @Get('/')
   ping() {
-    return 'pong';
+    return 'Welcome to my Movie API';
   }
 }
